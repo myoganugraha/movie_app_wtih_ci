6584494582d693ffb3ab08acc9071aa7849889bd
M. Yoga Nugraha <myoganugraha@gmail.com>
2024-04-08 15:29:07 +0800

    chore: init project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.movie_app_with_ci
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:movie_app_with_ci/app/app.dart';
+import 'package:movie_app_with_ci/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
